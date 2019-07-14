7f66f5594a48cd2ede9e0a526278848e1c2ef856
Ihar <yb.igrok.by@gmail.com>
2019-07-14 14:30:31 +0300

    feat: implement CRUD for users collection

--- server/routes/users_routes.js
+++ server/routes/users_routes.js
@@ -1,8 +1,46 @@
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+
+    const usersDB = db.db('Grok');
+
+    app.get('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        usersDB.collection('users').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(item);
+            }
+        })
+    });
+
+    app.delete('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        usersDB.collection('users').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(`User ${id} deleted`);
+            }
+        })
+    });
+
+    app.get('/users', (req, res) => {
+        usersDB.collection('users').find().toArray((err, items) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(items);
+            }
+        })
+    });
+
+
     app.post('/users', (req, res) => {
-        // res.send("Hello there");
         const user = {name: req.body.name, gender: req.body.gender};
-        const usersDB = db.db('Grok');
         usersDB.collection('users').insert(user, (err, result) => {
             if (err) {
                 res.send({'error': 'An error has occured'});
@@ -10,5 +48,18 @@ module.exports = function(app, db) {
                 res.send(result.ops[0]);
             }
         });
+    });
+
+    app.put('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        const user = {name: req.body.name, gender: req.body.gender};
+        usersDB.collection('users').update(details, user, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(user);
+            }
+        })
     });
 };
