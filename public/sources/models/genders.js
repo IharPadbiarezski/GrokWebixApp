28a3d6dda34474b0af81180b7294cba7b65a54f9
Ihar <yb.igrok.by@gmail.com>
2019-07-15 12:37:42 +0300

    feat: create common datatable

--- public/sources/models/genders.js
+++ public/sources/models/genders.js
@@ -1,6 +1,8 @@
 export const genders = new webix.DataCollection({
-	data: [
-		{id: 1, value: "male"},
-		{id: 2, value: "female"}
-	]
+	url: "http://localhost:3000/genders/",
+	save: "rest->http://localhost:3000/genders/"
+	// data: [
+	// 	{id: 1, value: "male"},
+	// 	{id: 2, value: "female"}
+	// ]
 });
--- public/sources/models/usersInfo.js
+++ public/sources/models/usersInfo.js
@@ -1,60 +1,62 @@
 export const usersInfo = new webix.DataCollection({
-	data: [
-		{
-			id: 1,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "tea"
-		},
-		{
-			id: 2,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "coffee"
-		},
-		{
-			id: 3,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "bear"
-		},
-		{
-			id: 4,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "vodka"
-		},
-		{
-			id: 5,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "tea"
-		},
-		{
-			id: 6,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "tea"
-		},
-		{
-			id: 7,
-			song: "In the ocean",
-			car: "BMW",
-			book: "Hello From Other Side",
-			movie: "Fast & Furious",
-			drink: "tea"
-		}
-	]
+	url: "http://localhost:3000/info/",
+	save: "rest->http://localhost:3000/info/"
+	// data: [
+	// 	{
+	// 		id: 1,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "tea"
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "coffee"
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "bear"
+	// 	},
+	// 	{
+	// 		id: 4,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "vodka"
+	// 	},
+	// 	{
+	// 		id: 5,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "tea"
+	// 	},
+	// 	{
+	// 		id: 6,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "tea"
+	// 	},
+	// 	{
+	// 		id: 7,
+	// 		song: "In the ocean",
+	// 		car: "BMW",
+	// 		book: "Hello From Other Side",
+	// 		movie: "Fast & Furious",
+	// 		drink: "tea"
+	// 	}
+	// ]
 });
