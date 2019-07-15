bda09f906b60f47e8640944b3e3cd5a7c560ba8f
Ihar <yb.igrok.by@gmail.com>
2019-07-15 19:52:34 +0300

    feat: add cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
