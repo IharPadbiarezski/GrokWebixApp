5a31266d210422743fba1752150004fb5eb742b8
Ihar <yb.igrok.by@gmail.com>
2019-07-16 09:16:37 +0300

    refactor: change template

--- public/sources/views/userTemplate.js
+++ public/sources/views/userTemplate.js
@@ -3,7 +3,7 @@ import {JetView} from "webix-jet";
 export default class UserTemplate extends JetView {
 	config() {
 		return {
-			template: "Place for Template",
+			template: "Place for Template in New Branch",
 			css: "webix_shadow_medium app_start"
 		}
 	}
