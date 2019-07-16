728c57a12eaaef41f7eb9f977c75aabe8234ad1d
Ihar <yb.igrok.by@gmail.com>
2019-07-16 09:41:26 +0300

    feat: make first two screen

--- public/sources/models/genders.js
+++ public/sources/models/genders.js
@@ -1,8 +1,8 @@
 export const genders = new webix.DataCollection({
-	url: "http://localhost:3000/api/v1/genders/",
-	save: "rest->http://localhost:3000/api/v1/genders/"
-	// data: [
-	// 	{id: 1, value: "male"},
-	// 	{id: 2, value: "female"}
-	// ]
+	// url: "http://localhost:3000/api/v1/genders/",
+	// save: "rest->http://localhost:3000/api/v1/genders/"
+	data: [
+		{id: 1, value: "male"},
+		{id: 2, value: "female"}
+	]
 });
--- public/sources/models/userData.js
+++ public/sources/models/userData.js
@@ -1,118 +1,118 @@
 export const userData = new webix.DataCollection({
-	url: "http://localhost:3000/api/v1/userdata/",
-	save: "rest->http://localhost:3000/api/v1/userdata/"
-	// data: [
-	// 	{
-	// 		id: 1,
-	// 		goal: "Go to Gym",
-	// 		userId: 3,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		goal: "New",
-	// 		userId: 2,
-	// 		hours: 1,
-	// 		reminder: "Do some",
-	// 		wish: "new car too"
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		goal: "Hi there",
-	// 		userId: 1,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 4,
-	// 		goal: "Go to Gym",
-	// 		userId: 2,
-	// 		hours: 5,
-	// 		reminder: "Do stuff",
-	// 		wish: "new black"
-	// 	},
-	// 	{
-	// 		id: 5,
-	// 		goal: "Go to the shop",
-	// 		userId: 4,
-	// 		hours: 4,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 6,
-	// 		goal: "Go to Gym",
-	// 		userId: 3,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 7,
-	// 		goal: "Go to Gym",
-	// 		userId: 1,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new bike"
-	// 	},
-	// 	{
-	// 		id: 8,
-	// 		goal: "Go to Gym",
-	// 		userId: 6,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 9,
-	// 		goal: "Newewfw",
-	// 		userId: 7,
-	// 		hours: 1,
-	// 		reminder: "Do some",
-	// 		wish: "new car too"
-	// 	},
-	// 	{
-	// 		id: 10,
-	// 		goal: "Hi there",
-	// 		userId: 7,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 11,
-	// 		goal: "Go to Gym",
-	// 		userId: 6,
-	// 		hours: 5,
-	// 		reminder: "Do stuff",
-	// 		wish: "new black"
-	// 	},
-	// 	{
-	// 		id: 12,
-	// 		goal: "Go to the shop",
-	// 		userId: 4,
-	// 		hours: 4,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 13,
-	// 		goal: "Go to Gym",
-	// 		userId: 5,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new car"
-	// 	},
-	// 	{
-	// 		id: 14,
-	// 		goal: "Go to Gym",
-	// 		userId: 6,
-	// 		hours: 5,
-	// 		reminder: "Do some crazy stuff",
-	// 		wish: "new bike"
-	// 	}
-	// ]
+	// url: "http://localhost:3000/api/v1/userdata/",
+	// save: "rest->http://localhost:3000/api/v1/userdata/"
+	data: [
+		{
+			id: 1,
+			goal: "Go to Gym",
+			userId: 3,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 2,
+			goal: "New",
+			userId: 2,
+			hours: 1,
+			reminder: "Do some",
+			wish: "new car too"
+		},
+		{
+			id: 3,
+			goal: "Hi there",
+			userId: 1,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 4,
+			goal: "Go to Gym",
+			userId: 2,
+			hours: 5,
+			reminder: "Do stuff",
+			wish: "new black"
+		},
+		{
+			id: 5,
+			goal: "Go to the shop",
+			userId: 4,
+			hours: 4,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 6,
+			goal: "Go to Gym",
+			userId: 3,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 7,
+			goal: "Go to Gym",
+			userId: 1,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new bike"
+		},
+		{
+			id: 8,
+			goal: "Go to Gym",
+			userId: 6,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 9,
+			goal: "Newewfw",
+			userId: 7,
+			hours: 1,
+			reminder: "Do some",
+			wish: "new car too"
+		},
+		{
+			id: 10,
+			goal: "Hi there",
+			userId: 7,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 11,
+			goal: "Go to Gym",
+			userId: 6,
+			hours: 5,
+			reminder: "Do stuff",
+			wish: "new black"
+		},
+		{
+			id: 12,
+			goal: "Go to the shop",
+			userId: 4,
+			hours: 4,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 13,
+			goal: "Go to Gym",
+			userId: 5,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new car"
+		},
+		{
+			id: 14,
+			goal: "Go to Gym",
+			userId: 6,
+			hours: 5,
+			reminder: "Do some crazy stuff",
+			wish: "new bike"
+		}
+	]
 });
